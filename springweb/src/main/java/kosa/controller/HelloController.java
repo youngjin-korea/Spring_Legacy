af4440b2c6309d75e114e86fafec02fca19c6bf3
kimyoungjin <youngjin1293@naver.com>
2024-08-06 11:56:04 +0900

    [feat] servlet.xml context:component 로 어노테이션 붙은 객체 모두 생성

--- springweb/src/main/java/kosa/controller/HelloController.java
+++ springweb/src/main/java/kosa/controller/HelloController.java
@@ -1,17 +1,23 @@
 package kosa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import kosa.service.HelloService;
+
 //컨트롤러 클래스가 되었다.
 @Controller
 public class HelloController {
+	
+	@Autowired 
+	private HelloService helloService;
 
 	@RequestMapping("/hello.do")
 	public ModelAndView hello() {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("message", "Hello Spring!!");
+		mav.addObject("message",helloService.getMessage());
 		mav.setViewName("hello");
 
 		return mav;
---
+++ springweb/src/main/java/kosa/service/HelloService.java
@@ -0,0 +1,12 @@
+package kosa.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+	public String getMessage() {
+		return "∏¿¡°«œººø‰";
+	}
+
+}
